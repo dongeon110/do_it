d0db214f77e8b2e6d1cc789eb0a2cecc0d69a9b7
dongeon110 <dongeon110@gmail.com>
2022-08-12 18:00:37 +0900

    220812 Study 360p

---
+++ src/chapter11_Class/Section1_Object_Class/StringEquals.java
@@ -0,0 +1,17 @@
+package chapter11_Class.Section1_Object_Class;
+// String과 Integer의 equals() Method
+public class StringEquals {
+	public static void main(String[] args) {
+		String str1 = new String("abc");
+		String str2 = new String("abc");
+		
+		System.out.println(str1 == str2); // F
+		System.out.println(str1.equals(str2)); // T
+		
+		Integer i1 = new Integer(100);
+		Integer i2 = new Integer(100);
+		
+		System.out.println(i1 == i2); // F
+		System.out.println(i1.equals(i2)); // T
+	}
+}
